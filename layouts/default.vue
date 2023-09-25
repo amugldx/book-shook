20153e44ef4e147f7fc5dad7e5471ae623f66186
amrmugl <meet.sagi.here@gmail.com>
2023-09-25 05:01:08 +0500

    initial setup with tailwind, cva, tw-merge added

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="bg-background w-screen h-screen">
+		<slot />
+	</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./components/**/*.{js,vue,ts}',
+		'./layouts/**/*.vue',
+		'./pages/**/*.vue',
+		'./plugins/**/*.{js,ts}',
+		'./app.vue',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				text: 'var(--text)',
+				background: 'var(--background)',
+				primary: 'var(--primary)',
+				secondary: 'var(--secondary)',
+				accent: 'var(--accent)',
+			},
+		},
+	},
+	plugins: [],
+};
