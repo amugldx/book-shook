70fffde75d6cb686577cc85eff7be87de396d849
amrmugl <meet.sagi.here@gmail.com>
2023-10-20 03:47:31 +0500

    added login route

---
+++ server/api/auth/login.post.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod';
+
+const bodySchema = z.object({
+	username: z.string(),
+	password: z.string(),
+});
+
+export default defineEventHandler(async event => {
+	const body = await readValidatedBody(event, bodySchema.parse);
+
+	if (!body) {
+		return sendError(
+			event,
+			createError({
+				statusMessage: 'invalid request body',
+				statusCode: 406,
+			}),
+		);
+	}
+
+	const user = await event.context.prisma.user.findUnique({
+		where: {
+			username: body.username,
+		},
+	});
+
+	if (!user) {
+		return sendError(
+			event,
+			createError({
+				statusMessage: 'user not found',
+				statusCode: 404,
+			}),
+		);
+	}
+
+	if (user?.password !== body.password) {
+		return sendError(
+			event,
+			createError({
+				statusMessage: 'password does not match',
+				statusCode: 406,
+			}),
+		);
+	}
+
+	setCookie(event, 'BookShookUser', user.id, {
+		httpOnly: true,
+	});
+
+	return {
+		id: user.id,
+		username: user.username,
+	};
+});
