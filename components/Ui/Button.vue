f309169c1fc720575a7554dd7d5d747d28dbfc52
amrmugl <meet.sagi.here@gmail.com>
2023-10-22 04:48:08 +0500

    disabled state added

--- components/Ui/Button.vue
+++ components/Ui/Button.vue
@@ -4,19 +4,22 @@
 
 	const buttonClass = computed(() => {
 		return twMerge(
-			cva('px-4 py-1 rounded-lg flex hover:shadow-md  justify-center items-center transition-all', {
-				variants: {
-					intent: {
-						primary: 'bg-primary-100 text-text-900 hover:bg-primary-200',
-						secondary: 'bg-secondary-100 text-text-900 hover:bg-secondary-200',
-						accent: 'bg-accent-100 text-text-900 hover:bg-accent-200',
-					},
-					size: {
-						icon: 'h-10 w-10 px-1 py-1',
-						large: 'h-10',
+			cva(
+				'px-4 py-1 disabled:cursor-not-allowed disabled:opacity-80 rounded-lg flex hover:shadow-md  justify-center items-center transition-all',
+				{
+					variants: {
+						intent: {
+							primary: 'bg-primary-100 text-text-900 hover:bg-primary-200',
+							secondary: 'bg-secondary-100 text-text-900 hover:bg-secondary-200',
+							accent: 'bg-accent-100 text-text-900 hover:bg-accent-200',
+						},
+						size: {
+							icon: 'h-10 w-10 px-1 py-1',
+							large: 'h-10',
+						},
 					},
 				},
-			})({
+			)({
 				intent: intent,
 				size: size,
 			}),
@@ -25,12 +28,14 @@
 
 	const {
 		intent = 'primary',
+		isLoading,
 		size,
 		type,
 	} = defineProps<{
 		intent: 'primary' | 'secondary' | 'accent';
 		size?: 'icon' | 'large';
 		type?: 'submit';
+		isLoading?: boolean;
 	}>();
 </script>
 
@@ -39,6 +44,7 @@
 		<button
 			:class="buttonClass"
 			:type="type"
+			:disabled="isLoading"
 			><slot
 		/></button>
 	</div>
