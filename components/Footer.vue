2da2bcb6edd14c5ba69a1f45e20953313a4273c9
amrmugl <meet.sagi.here@gmail.com>
2023-12-28 18:49:38 +0500

    footer added

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div
+		class="absolute w-3/4 lg:w-1/2 h-10 bottom-6 left-[50%] translate-x-[-50%] bg-text-950 text-text-50 dark:bg-text-50 dark:text-text-950 rounded-lg flex justify-center items-center">
+		<p>Made with &#129505 by
+			<a href="https://github.com/amugldx" target="_blank">@amugldx</a></p>
+	</div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,6 @@
 		class="min-h-screen p-3 md:p-4 lg:p-5 xl:p-6 2xl:p-7 bg-background-50 dark:bg-background-950">
 		<Navbar />
 		<slot />
+		<Footer />
 	</div>
 </template>
